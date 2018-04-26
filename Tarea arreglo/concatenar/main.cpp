2b9c40475dc4ef44b1492f71250b23b0442c4480
EninX <37408757+EninX@users.noreply.github.com>
2018-04-26 09:21:23 -0500

    Update main.cpp

--- Tarea arreglo/concatenar/main.cpp
+++ Tarea arreglo/concatenar/main.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 void concatenar(char *lista1,char *lista2){
-	int i=0;
-	while(lista1[i]!='\0')
+	void concatenar(char *lista1,char *lista2){
+	int cont=0;
+	while(lista1[cont]!='\0')
 		cont++;
 	for(int i=0;lista2[i]!='\0';i++){
-		lista1[i]=lista2[i];
-		i++;
+		lista1[i+cont]=lista2[i];
 	}
 }
 
