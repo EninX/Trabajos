a66d5289bb734c3db0cae4b4f444e32fd1f36323
EninX <37408757+EninX@users.noreply.github.com>
2018-09-19 20:34:43 -0500

    Update main.cpp

--- euler/poblema 4/main.cpp
+++ euler/poblema 4/main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <string>
 using namespace std;
-void multiples(int num)
-{
-    int sum=0;
-    for(int x=0; x<num;x++)
-    {
-        if(x%3==0)
-        {
-            sum=sum+x;
-        }
-        else if(x%5==0)
-        {
-            sum=sum+x;
-        }
-    }
-    cout<<"la suma es: "<<sum;
-
-}
 int main()
 {
-  int number=1000;
-  multiples(number);
+
+   int p=0, rev=0, rem=0, max1=0, max=0;
+    for (int i = 999; i >= 100; i--)
+   {
+        for (int j = 999; j >= 100; j--)
+       {
+            p = i * j;
+            while(p!=0)
+            {
+               rem = p % 10;
+               rev = rev * 10 + rem;
+               p = p / 10;
+           }
+           if (rev == (i*j))
+           {
+               max1 = rev;
+               if (max1 > max)
+               {
+                   max = max1;
+                }
+           }           rem = 0;
+            rev = 0;
+
+        }
+   }
+    cout <<"el mayor palandomo del producto de 3 digitos es: "<<max<<endl;
+    return 0;
+}
